f86663ed0182061ef477b074850db6704f713dff
Adib Nasyrul Shodiq <adib.shodiq@gmail.com>
2024-11-02 15:24:31 +0700

    fix route

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,6 @@ use App\Http\Controllers\IndexController;
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/do-kecil', [IndexController::class, 'index']);
+Route::get('/', [IndexController::class, 'index']);
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
